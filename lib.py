8ce92cfc64ac175537f58aec894d5255570ede12
Zack Scholl <zack.scholl@gmail.com>
2015-10-03 11:20:26 -0400

    Don't need settings anymore

--- lib.py
+++ lib.py
@@ -6,9 +6,6 @@ from multiprocessing import Pool, cpu_count
 
 import requests
 
-from settings import *
-
-
 
 def isurl(s):
     if re.match(r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))', s):
